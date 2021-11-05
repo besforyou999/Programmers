dc2a7399f191d1dbdbddb1ceb36dc44ed36debf0
Sangmin <besforyou2@gmail.com>
2021-11-05 22:32:02 +0900

    add comments

--- makePrime/solution.java
+++ makePrime/solution.java
@@ -19,12 +19,14 @@ class Solution {
         return answer;
     }
     
-    public int is_prime(int number) {
+    public int is_prime(int number) { // 소수가 아니면 0, 소수면 1 반환
         
-		if(number < 2) return 0;
+		if(number < 2) return 0; // 0 또는 1은 소수가 아니다
         
-		if(number == 2) return 1;
+		if(number == 2) return 1; // 2는 소수
         
+		// 자기자신의 루트까지 자신을 나누었을때 나누어 떨어지면 소수가 아님
+		// 나누어 떨어지지 않으면 소수. 시간복잡도를 O(N)에서 O(sqrt(N))까지 떨어뜨린다
 		for(int i = 2; i <= Math.sqrt(number); i++) {
 			if(number % i == 0) return 0;
 		}
