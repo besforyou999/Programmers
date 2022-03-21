71a780d10e278c14ae118b457c1a6aaeb997e28c
Sangmin <besforyou2@gmail.com>
2022-03-21 21:24:26 +0900

    add open_chatroom js version 2

---
+++ 2019_KAKAO_BLIND_RECRUITMENT/Open_Chatroom/Main2.js
@@ -0,0 +1,52 @@
+function makePair(userId, op) {
+	return {
+		userId: userId,
+		op: op
+	};
+}
+
+function solution(record) {
+    var answer = [];
+
+    let map = new Map();
+    let array = new Array();
+
+    for (let i = 0 ; i < record.length ; i++) {
+        let line = record[i];
+        let tokens = line.split(' ');
+        let op , userId, nickname;
+        op = tokens[0];
+        userId = tokens[1];
+        if (tokens.length == 3) { nickname = tokens[2]; }
+        
+        if ( op == "Enter") {
+            array.push(makePair(userId, op));
+            map.set(userId, nickname);
+        } else if ( op == "Leave") {
+            array.push(makePair(userId, op));
+        } else if (op == "Change") {
+            map.set(userId, nickname);
+        }
+    }
+
+    for (let i = 0 ; i < array.length ; i++) {
+        let line = "";
+        let pair = array[i];
+        let userId = pair.userId;
+        let operation = pair.op;
+        let nickname = map.get(userId);
+
+        line += ( nickname + "님이 ");
+        if (operation == "Enter") {
+            line += "들어왔습니다.";
+        } else if (operation == "Leave") {
+            line += "나갔습니다.";
+        }
+        answer.push(line);
+    }
+    
+    return answer;
+}
+
+
+
