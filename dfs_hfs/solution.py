c655242f5b2944bb425d482dd4a576660b698ee5
Sangmin <besforyou2@gmail.com>
2022-08-18 14:29:08 +0900

    코딩테스트 연습 > 깊이/너비 우선 탐색(DFS/BFS) > 타겟 넘버 (Python)

---
+++ dfs_hfs/solution.py
@@ -0,0 +1,14 @@
+def solution(numbers, target):
+    res = [0]
+    def dfs(i, n, l):
+        if i == len(l):
+            if n == target:
+                res[0] += 1
+            return
+        v = l[i] 
+        dfs(i + 1, n + v, l)
+        dfs(i + 1, n - v, l)
+
+    dfs(0, 0, numbers)
+
+    return res[0]
